8e1129b1d6843ef0d400fbd28cfdc502084dacb0
morgiz <99254256+morgiz@users.noreply.github.com>
2026-01-19 20:11:36 +0100

    Delete simulator directory

--- simulator/test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-route = 'fallback_points.txt'
-data = np.loadtxt(route)
-north = []
-east = []
-for i in range(0, (int(np.size(data) / 2))):
-    north.append(data[i][0])
-    east.append(data[i][1])
